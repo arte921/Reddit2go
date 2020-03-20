15cc66ea263c1dc67c6c2a164986e05293e6f001
arte921 <ardjanve@gmail.com>
2020-03-20 17:23:27 +0100

    work

--- app/src/main/java/arte921/reddit2go/utility.kt
+++ app/src/main/java/arte921/reddit2go/utility.kt
@@ -8,3 +8,16 @@ const val STORAGE_ERROR = LOADING + 1
 const val MEDIA_ONLY = 0
 const val THREAD = MEDIA_ONLY + 1
 const val MEDIA_OR_THREAD = THREAD + 1
+
+const val WRITE_PERMISSION_CODE = 0
+
+const val NEW = 0
+const val TOP = NEW + 1
+const val HOT = TOP + 1
+
+const val HOUR = 0
+const val DAY = HOUR + 1
+const val WEEK = DAY + 1
+const val MONTH = WEEK + 1
+const val YEAR = MONTH + 1
+const val ALL = YEAR + 1
